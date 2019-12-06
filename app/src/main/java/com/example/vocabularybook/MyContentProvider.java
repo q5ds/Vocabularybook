33f49a845cba3943f509f504d96201538e7d8b60
LL <LL@bistu.edu.cn>
2019-12-06 16:13:14 +0800

    commit

--- app/src/main/java/com/example/vocabularybook/MyContentProvider.java
+++ app/src/main/java/com/example/vocabularybook/MyContentProvider.java
@@ -9,25 +9,25 @@ import android.net.Uri;
 import android.provider.ContactsContract;
 import android.util.Log;
 
+import static com.example.vocabularybook.Background.database;
+
 public class MyContentProvider extends ContentProvider
 {
     private final int DIR = 0;
     private final int ITEM = 1;
     private final String AUTHORITY = "com.example.vocabularybook.provider";
+    private static UriMatcher uriMatcher;
+    public MyContentProvider(){
 
-    private UriMatcher uriMatcher;
-
-    public MyContentProvider()
-    {
-        this.uriMatcher = new UriMatcher((UriMatcher.NO_MATCH));
-        uriMatcher.addURI(AUTHORITY, "Database", DIR);
-        uriMatcher.addURI(AUTHORITY, "Database/#", ITEM);
     }
+
 
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
         // Implement this to handle requests to delete one or more rows.
-        throw new UnsupportedOperationException("Not yet implemented");
+        int deletedRows = 0;
+        deletedRows = database.delete("Database", selection, selectionArgs);
+        return deletedRows;
     }
 
     @Override
@@ -40,7 +40,9 @@ public class MyContentProvider extends ContentProvider
     @Override
     public Uri insert(Uri uri, ContentValues values) {
         // TODO: Implement this to handle requests to insert a new row.
-        throw new UnsupportedOperationException("Not yet implemented");
+        long newId = database.insert("Database", null, values);
+        Uri returnUri = Uri.parse("content://" + AUTHORITY + "Database/" + newId);
+        return returnUri;
     }
 
     @Override
@@ -53,22 +55,8 @@ public class MyContentProvider extends ContentProvider
     public Cursor query(Uri uri, String[] projection, String selection,
                         String[] selectionArgs, String sortOrder)
     {
-        DatabaseOpenHelper openHelper = new DatabaseOpenHelper(getContext(), "VocabularyBook.db", null, 1);
-        SQLiteDatabase database = openHelper.getReadableDatabase();
-        int matachRes = uriMatcher.match(uri);
         Cursor cursor = null;
-        if(matachRes == DIR)
-        {
-            final String SQL = "SELECT * FROM 'Database'";
-            Log.d("MyContentProvider", "executing SQL for content provider: " + SQL);
-            cursor = database.rawQuery(SQL, null);
-        }
-        else if(matachRes == ITEM)
-        {
-            final String SQL = "SELECT * FROM 'Database' WHERE ENG = ?";
-            Log.d("MyContentProvider", "executing SQL for content provider: " + SQL);
-            cursor = database.rawQuery(SQL, selectionArgs);
-        }
+        cursor = database.query("Database", projection, selection, selectionArgs, null, null, sortOrder);
         return cursor;
     }
 
@@ -76,6 +64,9 @@ public class MyContentProvider extends ContentProvider
     public int update(Uri uri, ContentValues values, String selection,
                       String[] selectionArgs) {
         // TODO: Implement this to handle requests to update one or more rows.
-        throw new UnsupportedOperationException("Not yet implemented");
+        int updateRows = 0;
+        updateRows = database.update("Database", values, selection, selectionArgs);
+
+        return updateRows;
     }
 }
